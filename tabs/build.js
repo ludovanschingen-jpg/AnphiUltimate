881f32c74abefaf7fc36104e5065f72825bfabc3
ludovanschingen-jpg <ludo.vanschingen@gmail.com>
2026-08-25 00:34:41 +0200

    Update build.js

--- tabs/build.js
+++ tabs/build.js
@@ -17,16 +17,15 @@
     const LEFT_SPECIALS = ['theater', 'thermal', 'library', 'lighthouse'];
     const RIGHT_SPECIALS = ['tower', 'statue', 'oracle', 'trade_office'];
     
-    // Coordonnées exactes et corrigées des sprites pour chaque bâtiment spécial et classique
+    // Grille de sprites 10x3 exacte et réalignée (50x50) sans obélisque
     const SPRITES = { 
-        main: [450, 0], lumber: [400, 0], stoner: [200, 50], ironer: [250, 0], 
-        storage: [250, 50], farm: [150, 0], barracks: [50, 0], docks: [100, 0], 
-        wall: [0, 100], academy: [0, 0], temple: [300, 50], market: [0, 50], 
-        hide: [200, 0], 
-        // Spéciaux Gauche corrigés (Théâtre, Thermes, Bibliothèque, Phare)
-        theater: [350, 0], thermal: [350, 50], library: [400, 50], lighthouse: [450, 50], 
-        // Spéciaux Droite corrigés (Tour, Statue, Oracle, Comptoir - Sans l'obélisque)
-        tower: [50, 50], statue: [150, 50], oracle: [100, 50], trade_office: [300, 0] 
+        academy: [0, 0], barracks: [50, 0], docks: [100, 0], farm: [150, 0], 
+        hide: [200, 0], ironer: [250, 0], trade_office: [300, 0], theater: [350, 0], 
+        lumber: [400, 0], main: [450, 0], 
+        market: [0, 50], tower: [50, 50], oracle: [100, 50], statue: [150, 50], 
+        stoner: [200, 50], storage: [250, 50], temple: [300, 50], thermal: [350, 50], 
+        library: [400, 50], lighthouse: [450, 50], 
+        wall: [0, 100] 
     };
 
     const FR_TO_ID = { 
@@ -237,7 +236,7 @@
             updateDesigner: (bid, val) => updateDesignerLevel(bid, val)
         };
 
-        log('BUILD', 'Module initialisé avec icônes spéciales parfaitement réalignées', 'info');
+        log('BUILD', 'Module initialisé avec sprites parfaitement réalignés', 'info');
     };
 
     module.isActive = function() {
