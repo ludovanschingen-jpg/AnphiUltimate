a1c20a52a352e1959587fdc84b966316524a0606
ludovanschingen-jpg <ludo.vanschingen@gmail.com>
2026-08-25 01:09:48 +0200

    Update build.js

--- tabs/build.js
+++ tabs/build.js
@@ -17,15 +17,16 @@
     const LEFT_SPECIALS = ['theater', 'thermal', 'library', 'lighthouse'];
     const RIGHT_SPECIALS = ['tower', 'statue', 'oracle', 'trade_office'];
     
-    // Grille de sprites 10x3 exacte et réalignée (50x50) sans obélisque
+    // Coordonnées de sprites corrigées et réalignées (sans l'obélisque)
     const SPRITES = { 
-        academy: [0, 0], barracks: [50, 0], docks: [100, 0], farm: [150, 0], 
-        hide: [200, 0], ironer: [250, 0], trade_office: [300, 0], theater: [350, 0], 
-        lumber: [400, 0], main: [450, 0], 
-        market: [0, 50], tower: [50, 50], oracle: [100, 50], statue: [150, 50], 
-        stoner: [200, 50], storage: [250, 50], temple: [300, 50], thermal: [350, 50], 
-        library: [400, 50], lighthouse: [450, 50], 
-        wall: [0, 100] 
+        main: [450, 0], lumber: [400, 0], stoner: [200, 50], ironer: [250, 0], 
+        storage: [250, 50], farm: [150, 0], barracks: [50, 0], docks: [100, 0], 
+        wall: [0, 100], academy: [0, 0], temple: [300, 50], market: [0, 50], 
+        hide: [200, 0], 
+        // Spéciaux Gauche réalignés
+        theater: [200, 50], thermal: [400, 50], library: [450, 50], lighthouse: [350, 0], 
+        // Spéciaux Droite réalignés (sans l'obélisque)
+        tower: [350, 50], statue: [150, 50], oracle: [100, 50], trade_office: [300, 0] 
     };
 
     const FR_TO_ID = { 
@@ -236,7 +237,7 @@
             updateDesigner: (bid, val) => updateDesignerLevel(bid, val)
         };
 
-        log('BUILD', 'Module initialisé avec sprites parfaitement réalignés', 'info');
+        log('BUILD', 'Module initialisé avec icônes spéciales réalignées et sans obélisque', 'info');
     };
 
     module.isActive = function() {
